240887b9e5f7a66e3027cd6225cc86cf7c8ed5bc
jtmlock <jtmlock@gmail.com>
2020-10-27 19:29:17 -0700

    AndroidPasrtagram

--- app/src/main/java/com/example/parstagram/activity_main.java
+++ app/src/main/java/com/example/parstagram/activity_main.java
@@ -1,11 +1,10 @@
 package com.example.parstagram;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class activity_main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
